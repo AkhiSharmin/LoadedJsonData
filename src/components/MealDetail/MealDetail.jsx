612ad116a07ed124f097e90bcd99e6a0fe6b577c
AkhiSharmin <akhisharmin100@gmail.com>
2025-03-14 15:05:10 +0600

    update

--- src/components/MealDetail/MealDetail.jsx
+++ src/components/MealDetail/MealDetail.jsx
@@ -2,12 +2,18 @@ import { useLoaderData } from "react-router-dom";
 
 const MealDetail = () => {
   const mealDetails = useLoaderData();
-  const { idMeal, strMeal, strMealThumb } = mealDetails;
+  const meal = mealDetails.meals?.[0];
+  const { strMealThumb, idMeal, strMeal } = meal;
+
+  //   if (!meal) {
+  //     return <h2>No meal found</h2>;
+  //   }
+
   return (
     <div className="postBox">
-      <img className="imgWidth" src={strMealThumb} alt="" />
+      <img className="imgWidth" src={strMealThumb} alt={meal.strMeal} />
       <h2>Meal ID: {idMeal}</h2>
-      <p>{strMeal}</p>
+      <h3>{strMeal}</h3>
     </div>
   );
 };
